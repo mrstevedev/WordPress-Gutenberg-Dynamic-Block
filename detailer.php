b35cc584a1c84913467c130a3ed4019181120ff7
Steven Polido <steven.polido@vistage.com>
2019-08-30 14:26:55 -0700

    Initial commit

---
+++ detailer.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Bootstrap file to launch the plugin.
+ *
+ * @wordpress-plugin
+ * Plugin Name: Detailer
+ * Plugin URI:  https://github.com/davidyeiser/detailer
+ * Description: Plugin to create custom Gutenberg block, Book Details.
+ * Version:     0.1
+ * Author:      David Yeiser
+ * Author URI:  https://davidyeiser.com/
+ * License:     GPL2+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace SpPluginGutenberg\Detailer;
+
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+// Gets this plugin's absolute directory path.
+function _get_plugin_directory() {
+  return __DIR__;
+}
+
+// Gets this plugin's URL.
+function _get_plugin_url() {
+  static $plugin_url;
+
+  if (empty($plugin_url)) {
+    $plugin_url = plugins_url(null, __FILE__);
+  }
+
+  return $plugin_url;
+}
+
+// Enqueue JS and CSS
+include __DIR__ . '/lib/enqueue-scripts.php';
